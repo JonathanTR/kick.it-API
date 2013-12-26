ab871a1592717109a7c40282625f83798e9b6680
JonathanTR <Jonathan.d.reilly@gmail.com>
2013-12-26 14:11:58 -0600

    JTR: add email validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,21 @@
 require 'spec_helper'
 
 describe User do
-  
-  describe 'on sign up' do
 
+  describe 'on sign up' do
+    
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password) }
     it { should validate_presence_of(:name) }
-    it { should allow_value('john@doe.co.uk').for(:email)}
-    it { should allow_value('john@doe.com').for(:email)}
-    it { should_not allow_value('john@').for(:email)}
-    it { should_not allow_value('john@com').for(:email)}
-    it { should_not allow_value('john.com').for(:email)}
+    it { should allow_value('john@doe.co.uk').for(:email) }
+    it { should allow_value('john@doe.com').for(:email) }
+    it { should_not allow_value('john@').for(:email) }
+    it { should_not allow_value('john@com').for(:email) }
+    it { should_not allow_value('john.com').for(:email) }
+    it 'should validate uniqueness of email' do
+      User.create(email: 'john.doe@example.com', password: 'pass1234', password_confirmation: 'pass1234', name: 'John Doe')
+      should validate_uniqueness_of(:email)
+    end
 
   end
 
